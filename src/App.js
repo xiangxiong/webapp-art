dccc190ac7e4173083d7e854ddb2a035db55647a
向雄 <xx090304@163.com>
2019-05-13 20:21:13 +0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React,{Component} from 'react';
 import {Provider} from 'react-redux';
 import {BrowserRouter,Route} from 'react-router-dom';
-import Login from './pages/login/index';
 import Home from './pages/home/index';
 import store from './store';
 
