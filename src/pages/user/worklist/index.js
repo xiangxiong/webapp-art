415a980dcc7a33c2c93de7c681069c9180eaf53f
胡海斌 <huhaibin@huhaibindeMacBook-Pro.local>
2019-05-23 21:14:56 +0800

    作品库

--- src/pages/user/worklist/index.js
+++ src/pages/user/worklist/index.js
@@ -3,6 +3,7 @@ import './index.scss';
 import PublicHeader from './../../../components/header';
 import {connect} from 'react-redux';
 import {getUserWorkActionDispatch} from '../store/actionCreators';
+import {pictureUrl} from '../../../utils/common';
 
 class WorkList extends PureComponent{
 
@@ -15,37 +16,46 @@ class WorkList extends PureComponent{
             PageSize:20
         });
     }
+
+    getWorkItem = (workData = {}) => {
+        const {ImageName = '', ProdName = '', LimitPrice = '', MarketPrice = ''} = workData;
+
+        return (
+            <div className="art-worklist__item">
+                <div className="art-worklist__item-img">
+                    <div style={{
+                        background: `url(${pictureUrl(ImageName)}) 0% 0% / cover`
+                    }}>
+                    </div>
+                </div>
+                <div className="art-worklist__item-content">
+                    <h3>{ProdName}</h3>
+                    <div>{`市场价：${LimitPrice}元`}</div>
+                    <div>{`库 存：${MarketPrice}件`}</div>
+                    <div>
+                        <span className="art-worklist__item-action">编辑</span>
+                        <span className="art-worklist__item-action">删除</span>
+                    </div>
+                </div>
+            </div>
+        );
+    };
     
     render(){
         return (
             <Fragment>
                 <PublicHeader title="作品库" bgColor="#E87908"/>
-                <div className="art-worklist__item">
-                    <div className="art-worklist__item-img">
-                        <div>
-                        </div>
-                    </div>
-                    <div className="art-worklist__item-content">
-                             <h3>景德镇紫砂壶</h3>
-                             <div>市场价：1998元</div>
-                             <div>库   存：578件</div>
-                             <div>
-                                 <span className="art-worklist__item-action">编辑</span>
-                                 <span className="art-worklist__item-action">删除</span>
-                             </div>
-                    </div>
-                </div>
+                {this.getWorkItem()}
             </Fragment>
         )
     }
 };
 
 const mapStateToProps = (state) => {
-    console.log('workList',state.workList);
     return {
         workList:state.workList
     }
-}
+};
 
 const mapDispatchToProps = dispatch => {
     return {
@@ -53,6 +63,6 @@ const mapDispatchToProps = dispatch => {
             dispatch(getUserWorkActionDispatch(params))
         }
     }
-}
+};
 
 export default connect(mapStateToProps,mapDispatchToProps)(WorkList);
