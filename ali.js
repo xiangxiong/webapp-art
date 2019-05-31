db19061a149bedfdbd0fa5818f5dcd96314b464e
向雄 <xx090304@163.com>
2019-05-31 11:26:35 +0800

    fix bug

--- ali.js
+++
@@ -1,4 +0,0 @@
- <script src="./upload/jquery.min.js"></script>
-    <script src="./upload/es6-promise.min.js"></script>
-    <script src="./upload/aliyun-oss-sdk5.3.1.min.js"></script>
-    <script src="./upload/aliyun-vod-upload-sdk1.5.0.min.js"></script>
