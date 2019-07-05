80155bc2663a58c395d3adb43149d0faa1e64925
向雄 <xx090304@163.com>
2019-07-05 15:26:40 +0800

    fix white page

--- src/Routes.js
+++ src/Routes.js
@@ -235,7 +235,7 @@ export default [
     },
     {
         path:"/masterDetail/:id",
-        component:LoadMasterDetail,
+        component:MasterDetail,
         key:'masterDetail'
     },
     {
